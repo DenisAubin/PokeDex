ca080902d8e30bbdc5bf6316f444ed61ccb382e8
Denis Aubin <denis.aubin@mythalesgroup.io>
2022-03-07 16:29:55 +0100

    Nouveau datadump

---
+++ SpringBootAPI/src/main/resources/PokeDataDump7_3_22.sql
@@ -0,0 +1,22 @@
+INSERT INTO pokedex.pokemon (id,name,type1,type2,img_url,evolves_to,evolves_from) VALUES
+	 (1,'Bulbizarre','PLANTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',2,NULL),
+	 (2,'Herbizarre','PLANTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',3,1),
+	 (3,'Florizarre','PLANTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',NULL,2),
+	 (4,'Salameche','FEU','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',5,NULL),
+	 (5,'Reptincel','FEU','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',6,4),
+	 (6,'Dracaufeu','FEU','VOL','https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',NULL,5),
+	 (7,'Carapuce','EAU','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',8,NULL),
+	 (8,'Carabaffe','EAU','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',9,7),
+	 (9,'Tortank','EAU','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png',NULL,8),
+	 (10,'Chenipan','INSECTE','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png',11,NULL);
+INSERT INTO pokedex.pokemon (id,name,type1,type2,img_url,evolves_to,evolves_from) VALUES
+	 (11,'Chrysacier','INSECTE','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png',12,10),
+	 (12,'Papilusion','INSECTE','VOL','https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',NULL,11),
+	 (13,'Aspicot','INSECTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png',14,NULL),
+	 (14,'Coconfort','INSECTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/014.png',15,13),
+	 (15,'Dardagnan','INSECTE','POISON','https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png',NULL,14),
+	 (16,'Roucoul','NORMAL','VOL','https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png',17,NULL),
+	 (17,'Roucoups','NORMAL','VOL','https://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png',18,16),
+	 (18,'Roucarnage','NORMAL','VOL','https://assets.pokemon.com/assets/cms2/img/pokedex/full/018.png',NULL,17),
+	 (19,'Rattata','NORMAL','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/019.png',20,NULL),
+	 (20,'Rattatac','NORMAL','','https://assets.pokemon.com/assets/cms2/img/pokedex/full/020.png',NULL,19);
